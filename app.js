0541e0ea77cb20d843259b2e55cf0af41b92497c
MFahadQureshi <155016268+MFahadQureshi@users.noreply.github.com>
2024-02-22 02:46:49 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,33 @@
+// chapter 1
+// Q=1
+alert("Wellcom")
+
+// Q=2
+var password = 'Error! Please enter your password.'
+alert(password)
+
+// Q=3
+var wellcom1 = 'Wellcom to js landing page \nHappy coding'
+alert(wellcom1)
+
+// Q=4
+var answer = 'Wellcom to js land...'
+alert(answer)
+
+var js = "Happy Coding!\n Prevent this page from creating additional dialogs."
+alert(js)
+
+// Q=5
+var five = "Hello... I can run JS through my web browser's console"
+alert(five)
+    // chapter 2
+var firstName = "Fahad"
+var lastName = "Qureshi"
+alert(firstName + " " + lastName)
+
+// chapter 3
+var age = 23
+alert(age)
+    // chapter 4
+var life =
+    alert(life)
