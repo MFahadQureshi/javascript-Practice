3c4ccb6aec2a932c9c9a3f677a778f4cd400bd4c
MFahadQureshi <155016268+MFahadQureshi@users.noreply.github.com>
2024-02-29 01:58:36 +0500

    add chapter 5 & 6

---
+++ chapter 5 & 6/app.js
@@ -0,0 +1,92 @@
+// Q=1
+var x = 25;
+var y = 7;
+var rsult1 = x % 7;
+
+console.log(rsult1);
+
+// Q=2
+var a = 15 + 10;
+var b = 4 - 2;
+var rsult2 = a * b;
+
+console.log(rsult2);
+
+// Q=3
+var c = 12;
+var d = 3;
+var result3 = c ** d;
+
+console.log(result3);
+// Q=4
+var e = 10;
+var f = 3;
+var result4 = e += f * 2;
+
+console.log(result4);
+// Q=5
+var g = 20;
+var h = 4;
+var result5 = g + 5 / h;
+
+console.log(result5);
+// Q=6
+var i = 6;
+var j = 3;
+var result6 = i * j;
+
+console.log(result6);
+// Q=7
+var k = 18;
+var l = 5;
+var result7 = k - l + 2;
+
+console.log(result7);
+// Q=8
+var m = 7;
+var n = 2;
+var result8 = m ** n;
+
+console.log(result8);
+// Q=9
+var o = 13;
+var p = 4;
+var result9 = o % (p + 2);
+
+console.log(result9);
+// Q=10
+var q = 16;
+var r = 3
+var result10 = q / 2 * r;
+
+console.log(result10);
+// Q=11
+var s = 25;
+var t = 3;
+var result11 = s - 1 / t;
+
+console.log(result11);
+// Q=12
+var u = 11;
+var v = 3;
+var result12 = u - v + 2;
+
+console.log(result12);
+// Q=13
+var w = 22;
+var x = 5;
+var result13 = w + (x ** 2);
+
+console.log(result13);
+// Q=14
+var y = 19;
+var z = 4;
+var result14 = y % (z + 1);
+
+console.log(result14);
+// Q=15
+var a = 10;
+var b = 3;
+var result15 = a * (b - 2);
+
+console.log(result15);
