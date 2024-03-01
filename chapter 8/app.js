4e76fcb97aa1428821e2e15f2cf71371cbc64152
MFahadQureshi <155016268+MFahadQureshi@users.noreply.github.com>
2024-03-02 01:47:47 +0500

    update chapter 7 and chapter 8

---
+++ chapter 8/app.js
@@ -0,0 +1,50 @@
+// Q=1
+var firstName = "Fahad";
+var lastName = "Qureshi"
+
+console.log(firstName + " " + lastName);
+// Q=2
+var city = "new";
+var cName = "York";
+
+console.log(`${city} ${cName}`);
+// Q=3
+var fruit = 'apple';
+var dish = "pie";
+
+console.log(`${fruit} ${dish}`);
+// Q=4
+var color = "black";
+var animal = "bird";
+
+console.log(`${color} ${animal}`);
+// Q=5
+var a = "JavaScript";
+var b = "is fun";
+
+console.log(`${a} ${b}`);
+// Q=6
+var c = "Open";
+var d = "Source";
+
+console.log(`${c} ${d}`);
+// Q=7
+var e = "Happy";
+var f = "Birthday";
+
+console.log(`${e} ${f}`);
+// Q=8
+var g = "Good";
+var h = "Have";
+
+console.log(`${g} ${h}`);
+// Q=9
+var i = "Web";
+var j = "Design";
+
+console.log(`${i} ${j}`);
+// Q=10
+var k = "Artificial";
+var l = "Neural";
+
+console.log(`${k} ${l}`);
